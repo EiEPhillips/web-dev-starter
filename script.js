ef9101d2a6b543b3e6e3c92b8697f00d9babec30
EiEPhillips <61015994+EiEPhillips@users.noreply.github.com>
2020-07-23 15:47:23 +0100

    New script

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-let uText = document.getElementById('userInput').value;
-
-function myTitleCase(uText){
-    uText = uText.toLowerCase().split(' ');
-    for (var i = 0; i < uText.length; i++){
-        uText[i] = uText[i].charAt(0).toUpperCase() + uText[i].slice(1);
-    }
-    return uText.join(' ');
-}
-
-console.log(myTitleCase(uText));
-
-let showMe = function() {
-    document.getElementById('textDisplay').innerHTML = myTitleCase;
+let userInput = document.getElementById('userInput').value;
+let getData = document.getElementById('userInput').innerHTML;
+userInput.onkeyup = function () {
+    let changeFirstLetter = userInput.replace(userInput.charAt(0), userInput.charAt(0).toUpperCase());
+    getData = changeFirstLetter;
 };
