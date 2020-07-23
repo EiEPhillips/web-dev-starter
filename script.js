a61c11f604ce1daeb108fc720d827f70621e0161
EiEPhillips <61015994+EiEPhillips@users.noreply.github.com>
2020-07-23 13:20:34 +0100

    Updates

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let userText = document.getElementById('userInput');
-let showText = document.getElementById('textDisplay');
+let uText = document.getElementById('userInput').value;
 
-function myFunction(){
-    let strap = userText.value;
-    strap.toLowerCase().split(' ');
-    for (var i = 0; i < strap.length; i++){
-        strap[i] = strap[i].charAt(0).toUpperCase() + strap[i].slice(1);
+function myTitleCase(uText){
+    uText = uText.toLowerCase().split(' ');
+    for (var i = 0; i < uText.length; i++){
+        uText[i] = uText[i].charAt(0).toUpperCase() + uText[i].slice(1);
     }
-    return strap.join(' ');
+    return uText.join(' ');
 }
 
-showText.text(`${strap}`);
+console.log(myTitleCase(uText));
+
+let showMe = function() {
+    document.getElementById('textDisplay').innerHTML = myTitleCase;
+};
