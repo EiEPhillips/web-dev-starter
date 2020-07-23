dd50997652d3a9fa8ecb2386b6ab16da5d8b2524
EiEPhillips <61015994+EiEPhillips@users.noreply.github.com>
2020-07-23 16:59:03 +0100

    updated script again

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let userInput = document.getElementById('userInput').value;
 let getData = document.getElementById('userInput').innerHTML;
-userInput.onkeyup = function () {
+userInput.onchange = function () {
     let changeFirstLetter = userInput.replace(userInput.charAt(0), userInput.charAt(0).toUpperCase());
-    getData = changeFirstLetter;
+    getInput = changeFirstLetter;
+    if(userInput > 5){
+        let isSlug = changeFirstLetter.replace(" ", "-");
+        getInput = isSlug;
+    }
 };
