0a55735b361678f08720d0b5b090eae98e631d9f
EiEPhillips <61015994+EiEPhillips@users.noreply.github.com>
2020-07-24 02:11:10 +0100

    Updated
    titleCase fixed;
    Working on slug and image

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ const displayText = (field) => {
   displayField.innerText = titleCase(field.value);
 };
 
-let displayField1 = document.getElementById("displays");
-// const userInput = document.getElementById("test");
+
+/*
+let slugify = require('slugify');
+let displayField1 = document.getElementById("displays")
 let displayTexter = (field) => {
-  //displayField1.innerText = field.value;
-  if(displayField1 > 5){
-    let isSlug = displayTexter.replace(" ", "-");
-    displayField1.innerText = isSlug;
-}
+  displayField1.innerText = slugify(field.value);
 };
+*/
+
+function slugify (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+  
+    // remove accents, swap ñ for n, etc
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
